8c6b8aabb2fe98c1982142171dc51af3fd0c11df
JBG <jamesbryangraves@gmail.com>
2015-01-02 14:29:30 +0100

    change areas to array of objects with coords

--- Kijkruimte/HUHWalk.m
+++ Kijkruimte/HUHWalk.m
@@ -31,7 +31,8 @@
 		
 		NSArray *areas = dictionary[@"areas"];
 		for(NSInteger i = 0; i < areas.count; i++) {
-			NSArray *coordinates = areas[i];
+			NSDictionary *area = areas[i];
+			NSArray *coordinates = area[@"coords"];
 			MKMapPoint points[coordinates.count / 2];
 			for(NSInteger j = 0, k = 0; j < coordinates.count; j+=2, k++) {
 				CLLocationCoordinate2D c = {[coordinates[j] doubleValue], [coordinates[j+1] doubleValue]};
